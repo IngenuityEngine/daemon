77efb584f41d9b5b131a0d727c5cde89ef622c81
Kenji Endo <colvin.endo@gmail.com>
2016-07-21 16:50:18 -0700

    test commands

--- test/test_daemon.py
+++ test/test_daemon.py
@@ -26,17 +26,58 @@ class test(tryout.TestSuite):
 			{
 				'name': 'test',
 				'command': 'echo \'hi\'',
-				'data': {'some':'data'},
-				'type': 'rendernodes',
+				'type': ['developer','workstation'],
 			}).execute()
 
 		print self.database.create(
 			'command',
 			{
-				'name': 'echo',
+				'name': 'echo hello',
 				'command': 'echo \'hello\'',
-				'data': {'some':'data'},
-				'type': 'rendernodes',
+				'type': ['developer'],
+				'priority': 40,
+				'enabled': True,
+			}).execute()
+
+		print self.database.create(
+			'command',
+			{
+				'name': 'echo yo',
+				'command': 'echo \'yo\'',
+				'type': ['developer'],
+				'enabled': False,
+			}).execute()
+
+		print self.database.create(
+			'command',
+			{
+				'name': 'ls',
+				'command': 'ls .',
+				'type': ['developer', 'workstation', 'render'],
+			}).execute()
+
+		print self.database.create(
+			'command',
+			{
+				'name': 'python says hi',
+				'command': 'python C:/ie/daemon/test/scripts/testCommand.py',
+				'type': 'developer',
+			}).execute()
+
+		print self.database.create(
+			'command',
+			{
+				'name': 'fake',
+				'command': 'fake fake fake this will fail',
+				'type': 'developer',
+			}).execute()
+
+		print self.database.create(
+			'command',
+			{
+				'name': 'also fake',
+				'command': 'also fail because super fake',
+				'type': ['developer', 'render'],
 			}).execute()
 
 	def findCommands(self):
@@ -47,7 +88,14 @@ class test(tryout.TestSuite):
 	# 		.where('updated','greater than', lastRun)
 	# 		.execute()
 
-		self.assertEqual(len(commands), 2)
+		# self.assertEqual(len(commands), 5)
+		self.assertTrue(len(commands))
+
+		# test cases:
+		# adding commands normally to the database (shoudl be able to find the right number of commands)
+		# call getCommands for those and see how many it returns
+		# get command info should parse commands with weird whitespace and return correctly
+		# call update, it should not run if commands are older than last run
 
 if __name__ == '__main__':
 	tryout.run(test)
